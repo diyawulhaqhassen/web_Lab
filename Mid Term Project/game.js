b8702d62aaba1946423c9636a26574149c22b7b9
Diya <dddd>
2021-10-29 21:17:24 -0500

    mid term project

--- Mid Term Project/game.js
+++ Mid Term Project/game.js
@@ -4,60 +4,61 @@ let submitAnswerElement=document.querySelector('#submit-answer')
 let userResult=document.querySelector('#result')
 let playAgainButton=document.querySelector('#play-again')
 let image=document.querySelector('img')
-let flagPicture=document.createElement('img')
+//let flagPicture=document.createElement('img')
 let userScore=document.querySelector('#score')
-
-let  scoreCounter=0
-
-
-//userResult.innerHTML=''
-
-//console.log(answer)
-
-// let randomStateNumber = Math.floor(Math.random() * statesData.length);// select random state
-//    let randomState=statesData[randomStateNumber]
-//
-//     let flagImage=document.createElement('img' )
-//
-//
-//     flagImage.src=randomState.state_flag_url
-// console.log(flagImage)
-// randomFlagElement.appendChild(flagImage)
-//
-//  let stateName=randomState.state
-// console.log(stateName)
-// console.log(randomState.state)
+let userQuestion=document.querySelector('#question')
 
 
 
 
-//console.log(randomStateFlag)
-
-// randomFlagElement.setAttribute('src',statesData[randomState])
-// //randomFlagElement.innerHTML='<img src=${randomState.state_flag_url}>'
-let url1 ='https://github.com/CivilServiceUSA/us-states/blob/master/data/states.json'
- let url2 ='https://state-info.herokuapp.com/api/state-list'
+//let url1 ='https://github.com/CivilServiceUSA/us-states/blob/master/data/states.json'
+ let stateListUrl ='https://state-info.herokuapp.com/api/state-list'
 function getState(){
-    fetch(url2).then(response=>response.json()).then(statesinfo=>{
-        let randomStateNumber = Math.floor(Math.random() * statesinfo.length);// select random state
-        let randomState=statesinfo[randomStateNumber]
-        let url =`https://state-info.herokuapp.com/api/info/${randomState}`
-        console.log(randomState)
 
-        fetch(url).then(response2=>response2.json()).then(statesDetail=>{
+    fetch(stateListUrl).then(response=>response.json()).then(statesinfo=>{
+        let randomStatesList=[]
 
-                flagPicture.src=statesDetail.state_flag_url
-                randomFlagElement.appendChild(flagPicture)
-                return randomState
+        for ( let x=0; x<5; x++) {
+            let randomStateNumber = Math.floor(Math.random() * statesinfo.length);// select random state
+            let randomState = statesinfo[randomStateNumber]
+
+            console.log(randomState)
+            randomStatesList.push(randomState)
+        }console.log(randomStatesList)
+        let  questionCounter=0
+    randomStatesList.forEach(function (selectedState){
+                let stateInfoUrl = `https://state-info.herokuapp.com/api/info/${selectedState}`
+
+        fetch(stateInfoUrl).then(response2=>response2.json()).then(statesDetail=>{
+            let ask ='For which state is the following flag? '
+            let question=document.createElement('P')
+            let flagImage=document.createElement('img')
+            let answerInput=document.createElement('input')
+
+            question.innerHTML=ask
+
+            flagImage.src=statesDetail.state_flag_url
+
+            randomFlagElement.append(flagImage)
+            //randomFlagElement2.appendChild(flagImage)
+
+            userQuestion.append(question)
+            userAnswerElement.append(answerInput)
+            //userQuestion2.appendChild(question)
+            //return randomState
             }
-        )
+        ) })
+
         submitAnswerElement.addEventListener('click',function() {
             //console.log(answer)
             console.log(randomState)
+            let  correctAnswer=0
             let answer=userAnswerElement.value
             if (answer.toLowerCase()===randomState.toLowerCase()){
-                userResult.innerHTML=`Your answer is Correct `.fontcolor('green')
-
+                userResult.innerHTML=`Your answer is correct `.fontcolor('green')
+                correctAnswer++
+                userScore.innerHTML = (correctAnswer)+" / "+randomStatesList.length;
+                userScore.innerHTML=`Score ${scoreCounter+1}`
             }else  {
                 userResult.innerHTML=`Your answer is wrong the correct answer is ${randomState} `.fontcolor('red')
 
@@ -65,19 +66,21 @@ function getState(){
         })
 
 
-
+// nextButton.addEventListener('click',function () {
+//
+// })
 
 
     })
 }
 
-//getState()
 
-for ( let x=0; x<5; x++){
+
+
 
     getState()
 
-}
+
 
 
      playAgainButton.addEventListener('click',function (){
